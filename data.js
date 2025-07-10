3eeb175c712c5da0f939f0ea3951d599d8a4f933
compacto300 <conquestchars@gmail.com>
2025-07-09 21:31:23 -0300

    Update data.js

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ const allDrops = [
     { week: 2, date: "2025-07-08", period: "13:00", member: "Suellen", item: "Chama de Condor", guild: "SUNNY", rarity: "red" },
     { week: 2, date: "2025-07-08", period: "21:00", member: "Shivu", item: "Pena de Condor", guild: "SUNNY", rarity: "purple" },
     { week: 2, date: "2025-07-08", period: "21:00", member: "Nyx", item: "Crystal of Chaos", guild: "SUNNY", rarity: "yellow" },
-    { week: 2, date: "2025-07-09", period: "13:00", member: "", item: "Só o Osso", guild: "SHOWTIME", rarity: "none" },
+    { week: 2, date: "2025-07-09", period: "13:00", member: "Balena", item: "Crystal of Chaos", guild: "SHOWTIME", rarity: "yellow" },
     { week: 2, date: "2025-07-09", period: "21:00", member: "", item: "Só o Osso", guild: "SHOWTIME", rarity: "none" },
 ];
 
